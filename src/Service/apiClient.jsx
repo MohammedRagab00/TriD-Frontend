822cbfa7fdba99c30cd703e6cfdcd072b2494978
Zeyad Ayman <zeyad4WOnly@gmail.com>
2025-09-23 19:43:03 +0200

    Changed baseURL

--- src/Service/apiClient.jsx
+++ src/Service/apiClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://trid-dtgpbjcyecekdea8.uaenorth-01.azurewebsites.net/api/v1",
+  baseURL: "https://trid-dtgpbjcyecekdea8.uaenorth-01.azurewebsites.net/",
   // headers: {
   //   "Content-Type": "application/json",
   // },
